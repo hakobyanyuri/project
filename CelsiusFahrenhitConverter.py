c3c6529d6680750bba5e04dd03d17435fe87e926
Yuri Hakobyan <157562742+hakobyanyuri@users.noreply.github.com>
2024-10-10 19:32:45 +0400

    CelsiusFahrenhitConverter.py

--- CelsiusFahrenhitConverter.py
+++ CelsiusFahrenhitConverter.py
@@ -1,3 +1,11 @@
-c = float(input("_"))
-f = (c * 9/5) + 32
-print(c, "°C --->", f, "°F")
+choice = input("Select conversion type:\n1: Celsius to Fahrenheit\n2: Fahrenheit to Celsius\n_ ")
+if choice == "1":
+    c = int(input("Enter the temperature in degrees Celsius:"))
+    f = (c * 9/5) + 32
+    print(c, "°C --->", f, "°F")
+elif choice == "2":
+    f = int(input("Enter temperature in degrees Fahrenheit:"))
+    c = (f - 32) * 5/9
+    print(f, "°F --->", c, "°C")
+else:
+    print("Incorrect choice.")
